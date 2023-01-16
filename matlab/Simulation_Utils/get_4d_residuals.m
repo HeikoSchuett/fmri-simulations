279575070b5e808ac189e291cf0f770544e70359
Heiko Schuett <heiko.schuett@nyu.edu>
2023-01-15 23:11:47 -0500

    typo

--- matlab/Simulation_Utils/get_4d_residuals.m
+++ matlab/Simulation_Utils/get_4d_residuals.m
@@ -5,6 +5,6 @@ function [E, nii_hdr] = get_4d_residuals(Dirs)
     for i = 2 : Dirs.n_res 
 %                 res = niftiread(Dirs.res_files{i});
         res = load_untouch_nii_img(Dirs.res_files{i});
-        E[:,:,:,i] = res;
+        E(:,:,:,i) = res;
     end
 end
