5f1d3929f88f4fc13619a7bf6f0958b3c5131598
Heiko Schuett <heiko.schuett@nyu.edu>
2023-01-15 23:19:11 -0500

    adjusting Nii interaction

--- matlab/Simulation_Utils/get_4d_residuals.m
+++ matlab/Simulation_Utils/get_4d_residuals.m
@@ -1,10 +1,11 @@
 function [E, nii_hdr] = get_4d_residuals(Dirs)
     nii_hdr = load_untouch_header_only(Dirs.res_files{1});
     E = load_untouch_nii_img(Dirs.res_files{1});
+    E = E.img;
     E = repmat(E, 1, 1, 1, Dirs.n_res)
     for i = 2 : Dirs.n_res 
 %                 res = niftiread(Dirs.res_files{i});
-        res = load_untouch_nii_img(Dirs.res_files{i});
-        E(:,:,:,i) = res;
+        res = load_untouch_nii(Dirs.res_files{i});
+        E(:,:,:,i) = res.img;
     end
 end
