8159997f8bbc255ef6225128b8883eb5875ae9e6
Heiko Schuett <heiko.schuett@nyu.edu>
2023-01-15 23:07:58 -0500

    small steps towards working

--- 04 Simulation Scripts/Server Scripts/Full_simulation.sh
+++ 04 Simulation Scripts/Server Scripts/Full_simulation.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-#
 #SBATCH --account=nklab          # The account name for the job.
-#SBATCH --job-name=full_sim    # The job name.
+#SBATCH --job-name=full_sim      # The job name.
 #SBATCH -c 8                     # The number of cpu cores to use.
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -16,13 +15,16 @@ module load matlab
 module load anaconda/3-5.3.1
 conda activate fmri-sim
 export PATH="/moto/home/hs3110/.conda/envs/fmri-sim/bin:$PATH"
+mkdir /tmp/$SLURM_JOB_ID
+mkdir /tmp/$SLURM_JOB_ID/data
 
-for (( time=1; time <= 2; time=time+1 )); do 
-    matlab -nodisplay -nosplash - nodesktop -r "cd /moto/home/ak4572/; try, run ('/moto/home/ak4572/Noise_shuffling_moto.m'); catch me, fprintf('%s / %s\n',me.identifier,me.message), end, exit"
-    matlab -nodisplay -nosplash - nodesktop -r "cd /moto/home/ak4572/; try, run ('/moto/home/ak4572/GLM_on_sim_moto.m'); catch me, fprintf('%s / %s\n',me.identifier,me.message), end, exit"
-    python 01-Pool_simulation_results_moto.py
-    python 02-Create_sim_pyrsa_dataset_moto.py
-    python 03-Create_sim_RDMs_moto.py
-    python 04-Test_sim_fixed_inference_moto.py
-    
-done
+cd /moto/home/hs3110/fmri-simulations/04\ Simulation\ Scripts/Server\ Scripts
+
+matlab -nodisplay -nosplash - nodesktop -r "cd /tmp/$SLURM_JOB_ID; try, run ('/moto/home/hs3110/fmri-simulations/04 Simulation Scripts/Server Scripts/Noise_shuffling_moto.m'); catch me, fprintf('%s / %s\n',me.identifier,me.message), end, exit"
+matlab -nodisplay -nosplash - nodesktop -r "cd /tmp/$SLURM_JOB_ID; try, run ('/moto/home/hs3110/fmri-simulations/04 Simulation Scripts/Server Scripts/GLM_on_sim_moto.m'); catch me, fprintf('%s / %s\n',me.identifier,me.message), end, exit"
+python 01-Pool_simulation_results_moto.py
+python 02-Create_sim_pyrsa_dataset_moto.py
+python 03-Create_sim_RDMs_moto.py
+python 04-Test_sim_fixed_inference_moto.py
+
+rm -rf /tmp/$SLURM_JOB_ID
--- matlab/Simulation_Utils/get_4d_residuals.m
+++ matlab/Simulation_Utils/get_4d_residuals.m
@@ -1,11 +1,10 @@
 function [E, nii_hdr] = get_4d_residuals(Dirs)
-    E = [];   
-    nii_hdr = [];
-        for i = 1 : Dirs.n_res 
+    nii_hdr = load_untouch_header_only(Dirs.res_files{1});
+    E = load_untouch_nii_img(Dirs.res_files{i});
+    E = repmat(E, 1, 1, 1, Dirs.n_res)
+    for i = 2 : Dirs.n_res 
 %                 res = niftiread(Dirs.res_files{i});
-                res = load_untouch_nii(Dirs.res_files{i});
-                E = cat(4, E, res);
-        end
-    nii_hdr = load_untouch_header_only(Dirs.res_files{i});
+        res = load_untouch_nii_img(Dirs.res_files{i});
+        E[:,:,:,i] = res;
+    end
 end
-
