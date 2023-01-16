da2ea82f4a287b48ed70541aeb0a14091e792f06
Heiko Schuett <heiko.schuett@nyu.edu>
2023-01-15 23:20:39 -0500

    one more

--- matlab/Simulation_Utils/get_4d_residuals.m
+++ matlab/Simulation_Utils/get_4d_residuals.m
@@ -1,6 +1,6 @@
 function [E, nii_hdr] = get_4d_residuals(Dirs)
     nii_hdr = load_untouch_header_only(Dirs.res_files{1});
-    E = load_untouch_nii_img(Dirs.res_files{1});
+    E = load_untouch_nii(Dirs.res_files{1});
     E = E.img;
     E = repmat(E, 1, 1, 1, Dirs.n_res)
     for i = 2 : Dirs.n_res 
