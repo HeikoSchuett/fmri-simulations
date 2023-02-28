e890acd8e86e3c7134e52a52570c6dfe5a570a41
Heiko Schütt <hs3110@columbia.edu>
2023-02-28 05:09:19 -0500

    updates on cluster

--- 04 Simulation Scripts/Server Scripts/Full_simulation.sh
+++ 04 Simulation Scripts/Server Scripts/Full_simulation.sh
@@ -5,20 +5,20 @@
 #SBATCH -c 8                     # The number of cpu cores to use.
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=120:00:00              # The time the job will take to run.
+#SBATCH --time=48:00:00              # The time the job will take to run.
 #SBATCH --mem-per-cpu=6gb        # The memory the job will use per cpu core.
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hs3110@columbia.edu
-
+#SBATCH --output=/moto/home/hs3110/fmri-simulations/slurm/full_sim_%a.out
 
 module load matlab
 module load anaconda/3-5.3.1
 conda activate fmri-sim
 export PATH="/moto/home/hs3110/.conda/envs/fmri-sim/bin:$PATH"
-mkdir /tmp/$SLURM_JOB_ID
+mkdir /moto/nklab/users/hs3110/tmp/$SLURM_JOB_ID
 
 export SOURCE='/moto/nklab/projects/ds001246_r/'
-export INTERMEDIATE="/tmp/$SLURM_JOB_ID"
+export INTERMEDIATE="/moto/nklab/users/hs3110/tmp/$SLURM_JOB_ID"
 export CODE='/moto/home/hs3110/fmri-simulations/04 Simulation Scripts/Server Scripts'
 export MATLAB_CODE='/moto/home/hs3110/fmri-simulations/matlab'
 
